ad3fbf776df60c1a7dd75fef5df9df4236bd10f4
ciro-t-t-ferreira <ferreira.cirott@gmail.com>
2024-03-01 16:02:39 -0300

    Uses the useState Rook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
+
 const Home = () => {
 
+    const [name, setName] = useState('Otavio Mesquita');
+
     function handleClick(){
-        console.log('click click mf')
+        setName('Jorge Romeu')
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
             <br />
+            <h1> { name } </h1>
             <button onClick={handleClick}>Click Here</button>
         </div>
       );
