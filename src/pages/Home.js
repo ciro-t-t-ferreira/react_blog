28dfb9f0bf78f2e51ae6134103c9a8afb76a50cd
ciro-t-t-ferreira <ferreira.cirott@gmail.com>
2024-03-02 19:59:59 -0300

    customize error message and show to user

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,22 +3,32 @@ import BlogList from "../components/BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null)
+    const [blogs, setBlogs]         = useState(null)
     const [isPending, setIsPending] = useState(true)
+    const [error, setError]         = useState(null)
 
     useEffect(() => { //the function fires everytime that this component is rendred
         fetch('http://localhost:8000/blogs')
         .then(res => {
+            if(!res.ok){
+                throw Error('Could not fetch data for that Resource')
+            }
             return res.json()
         })
         .then(data =>{
             setBlogs(data)
+            setIsPending(false)
+            setError(false)
+        })
+        .catch(err => { //catch error in case the fetch fails
+            setError(err.message)
             setIsPending(false)
         })
     }, [])
 
     return (
         <div className="home">
+            {error && <div>{ error }</div>}
             {isPending && <div>Loading...</div> }
             {blogs && <BlogList blogs={ blogs } title="All Blogs"/>}
         </div>
