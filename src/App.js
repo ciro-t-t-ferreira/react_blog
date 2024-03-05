a6719272f0a5bb7c20bfd341e9cc9900b41f75d6
ciro-t-t-ferreira <ferreira.cirott@gmail.com>
2024-03-05 16:06:57 -0300

    implements catch all error page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import BlogDetails from './components/BlogDetais';
 import NavBar from './components/NavBar';
+import NotFound from './components/NotFound';
 import Create from './pages/Create';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
@@ -20,6 +21,13 @@ function App() {
           <Route path="/blogs/:id"
               element={<BlogDetails />}>
           </Route>
+          <Route path="/404"
+              element={<NotFound />}>                
+          </Route>
+
+          {/* Catch all route*/}
+          <Route path="*"
+              element={<Navigate to="/404"/>}></Route>
         </Routes> 
         </div>
       </div>
